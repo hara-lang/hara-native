fe3e2f0c991aee4f2248b361601803ab4f18e460
Chris Zheng <zcaudate@users.noreply.github.com>
2026-09-01 09:14:07 +1000

    release: prepare native 0.1.16

--- core/java/src/main/java/hara/kernel/NativeMain.java
+++ core/java/src/main/java/hara/kernel/NativeMain.java
@@ -32,7 +32,7 @@ public class NativeMain {
     }
 
     if ("version".equals(command) || "--version".equals(command) || "-V".equals(command)) {
-      System.out.println("hara-native-jvm 0.1.15");
+      System.out.println("hara-native-jvm 0.1.16");
       return;
     }
 
