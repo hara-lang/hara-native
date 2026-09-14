8ac07893ed98eb443ca03b6bcf04f48f2785dacd
Chris Zheng <zcaudate@users.noreply.github.com>
2026-09-14 11:18:40 +1000

    release: prepare native 0.1.31

--- core/java/src/main/java/hara/truffle/HaraServer.java
+++ core/java/src/main/java/hara/truffle/HaraServer.java
@@ -485,7 +485,7 @@ public final class HaraServer implements AutoCloseable {
         "SERVER",
         "HARA",
         "VERSION",
-        "0.1.30",
+        "0.1.31",
         "PROTO",
         (long) protocol,
         "RUNTIME",
