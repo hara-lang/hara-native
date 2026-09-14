e108e65b6af95ef9bb0b748a9921464653057b8a
Chris Zheng <zcaudate@users.noreply.github.com>
2026-09-14 17:21:21 +1000

    release: prepare native 0.1.32

--- core/java/src/main/java/hara/kernel/NativeMain.java
+++ core/java/src/main/java/hara/kernel/NativeMain.java
@@ -32,7 +32,7 @@ public class NativeMain {
     }
 
     if ("version".equals(command) || "--version".equals(command) || "-V".equals(command)) {
-      System.out.println("hara-native-jvm 0.1.31");
+      System.out.println("hara-native-jvm 0.1.32");
       return;
     }
 
